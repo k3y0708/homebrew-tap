f76cc763284e478f61c9d84a965d5889e7a5139c
k3y0708 <keyvan@atashfaraz.de>
2023-05-09 22:35:57 +0200

    ⬆️ Otter

--- Formula/otter.rb
+++ Formula/otter.rb
@@ -1,15 +1,17 @@
 class Otter < Formula
   desc "Otter is a small shell written in go"
-  version "v0.0.1"
+  version "v0.0.2"
   homepage "https://github.com/k3y0708/otter"
   url "https://github.com/k3y0708/otter/archive/refs/tags/#{version}.tar.gz"
-  sha256 "79d790a9bca260a4aa9ab9ae12b8d0c1523b4650ad38c1ef6cb7f8c1d4ce3649"
+  sha256 "f44841e57759a4cdfe694282b7ded456d14e1bb12df5aa8fef08b5c34825027d"
   license "GNU GPLv3"
   head "https://github.com/k3y0708/otter.git"
   depends_on "go" => :build
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    cd "src" do
+      system "go", "build", *std_go_args(ldflags: "-s -w")
+    end
   end
 end
